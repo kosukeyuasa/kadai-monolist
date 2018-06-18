6db22b0df97d762e55f49462bb57699207be9e64
kosuke.yuasa <kosuke.yuasa@rakuten.com>
2018-06-18 06:11:31 +0000

    ranking

---
+++ resources/views/ranking/want.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Wantランキング</h1>
+    @include('items.items', ['items' => $items])
+@endsection
+@extends('layouts.app')
+
+@section('content')
+    <h1>Wantランキング</h1>
+    @include('items.items', ['items' => $items])
+@endsection
