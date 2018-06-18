eed6f79e88c318dc5d339c90d971e083cc1b07f9
kosuke.yuasa <kosuke.yuasa@rakuten.com>
2018-06-18 06:26:07 +0000

    want rank

--- resources/views/ranking/want.blade.php
+++ resources/views/ranking/want.blade.php
@@ -4,9 +4,4 @@
     <h1>Wantランキング</h1>
     @include('items.items', ['items' => $items])
 @endsection
-@extends('layouts.app')
 
-@section('content')
-    <h1>Wantランキング</h1>
-    @include('items.items', ['items' => $items])
-@endsection
