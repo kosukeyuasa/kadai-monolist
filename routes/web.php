f09035c2fb8b30c7f52cd45209f5035beec97638
kosuke.yuasa <kosuke.yuasa@rakuten.com>
2018-06-19 01:39:28 +0000

    route2

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Route::group(['middleware' => ['auth']], function () {
     Route::post('want', 'ItemUserController@want')->name('item_user.want');
     Route::delete('want', 'ItemUserController@dont_want')->name('item_user.dont_want');
     Route::resource('users', 'UsersController', ['only' => ['show']]);
-);
 
 
     Route::resource('items', 'ItemsController', ['only' => ['create', 'show']]);
